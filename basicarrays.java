0e9d82bd7b5119b71050e89cf19a4a952cdc5e32
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-05-21 23:43:38 +0530

    Create basicarrays.java

---
+++ basicarrays.java
@@ -0,0 +1,22 @@
+package com.akshat;
+import java.util.*;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+//		int[] marks = new int[3];		//Way to create arrays
+//		marks[0] = 97;					/Store marks
+//		marks[1] = 96;
+//		marks[2] = 99;
+		
+		
+		int marks[] = {10,11,12};		//Another way to create array
+
+		for(int i = 0;i< 3;i++) {			//Loops to print arrays
+			System.out.println(marks[i]);
+		}
+		}
+
+	}
+
+
